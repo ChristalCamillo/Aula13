88d882581811f3c351bbc937194839663e1c4721
christal.camillo <christal.b.camillo@gmail.com>
2022-04-14 16:36:10 -0300

    ex01

---
+++ src/Exercicio1.kt
@@ -0,0 +1,14 @@
+//Faça um programa que leia 5 valores inteiros, armazene-os em um
+//vetor, calcule e apresente a soma destes valores.
+
+fun main() {
+    var listavalores = IntArray(5)
+    println("Digite 5 valores inteiros para saber sua soma: ")
+    //para cada valor do tipo inteiro dentro de listavalores, vou rodar o readln
+    for(valor: Int in listavalores){
+        readln().toInt()
+    }
+    for(listavalores in listavalores)
+
+
+}
